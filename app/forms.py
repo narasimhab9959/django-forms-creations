a696ca10921e30f4fa746d22ca34cd79d1f8e6ae
narasimhab9959 <narasimhab8555@gmail.com>
2023-07-06 19:00:02 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,12 @@
 from django import forms
-
+c=[('python','PYTHON'),('java','JAVA',),('django','DJANGO')]
 g=[('male',"MALE"), ('female',"FEMALE")]
 class SignUpForm(forms.Form):
     name=forms.CharField(max_length=100)
     age=forms.IntegerField()
     email=forms.EmailField()
     Password=forms.CharField(max_length=100,widget=forms.PasswordInput)
-    gender=forms.ChoiceField(choices=g)
+    #gender=forms.ChoiceField(choices=g)
+    gender=forms.ChoiceField(choices=g,widget=forms.RadioSelect)
+    #Course=forms.MultipleChoiceField(choices=c)
+    Course=forms.MultipleChoiceField(choices=c,widget=forms.CheckboxSelectMultiple)
